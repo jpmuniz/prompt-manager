5222376dd22e2b5b1aecc2b95af9ea6f67c2c902
Joao Pedro <jpmuniz88@gmail.com>
2026-04-04 11:03:34 -0300

    Implements home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased bg-gray-900 text-white`}>
-        {children}
+    <html lang="pt-BR" className={`${inter.variable} min-h-dvh bg-gray-900`}>
+      <body
+        className={`${inter.className} antialiased min-h-dvh bg-gray-900 text-white`}
+      >
+        <section className="flex h-screen">
+          <main className="relative flex-1 overflow-auto min-w-0">
+            <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
+              {children}
+            </div>
+          </main>
+        </section>
       </body>
     </html>
   );
